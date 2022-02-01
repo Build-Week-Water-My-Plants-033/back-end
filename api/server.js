e64baea30ad47d14f1e1c079ff23c4ab7ec1700c
Jon_Em <45055471+Jmz0127@users.noreply.github.com>
2022-02-01 16:52:49 -0500

    added plants and auth files and updated server

--- api/server.js
+++ api/server.js
@@ -1,28 +1,23 @@
 const express = require('express')
 const helmet = require('helmet')
 const cors = require('cors')
-const db = require('./data/db-config')
+//const authRouter = require('')
+//const plantsRouter = require('')
 
-function getAllUsers() { return db('users') }
-
-async function insertUser(user) {
-  // WITH POSTGRES WE CAN PASS A "RETURNING ARRAY" AS 2ND ARGUMENT TO knex.insert/update
-  // AND OBTAIN WHATEVER COLUMNS WE NEED FROM THE NEWLY CREATED/UPDATED RECORD
-  const [newUserObject] = await db('users').insert(user, ['user_id', 'username', 'password'])
-  return newUserObject // { user_id: 7, username: 'foo', password: 'xxxxxxx' }
-}
 
 const server = express()
 server.use(express.json())
 server.use(helmet())
 server.use(cors())
 
-server.get('/api/users', async (req, res) => {
-  res.json(await getAllUsers())
-})
 
-server.post('/api/users', async (req, res) => {
-  res.status(201).json(await insertUser(req.body))
-})
+// server.use("/api/auth", authRouter); UNCOMMENT THIS OUT ONCE AUTH ROUTER IS SET UP - DONT FORGET TO REQUIRE IT UP TOP!
+// server.use("/api/plants", plantsRouter); UNCOMMENT THIS OUT ONCE PLANTS ROUTER IS SET UP - DONT FORGET TO REQUIRE IT UP TOP!
 
+server.use((err, req, res, next) => { // eslint-disable-line
+  res.status(err.status || 500).json({
+    message: err.message,
+    stack: err.stack,
+  });
+});
 module.exports = server
