8968d6cb2e0c2d784f32d35b716bbb522f9e3e72
Jon_Em <45055471+Jmz0127@users.noreply.github.com>
2022-02-01 16:16:27 -0500

    fixed seed error

--- api/data/seeds/02-data.js
+++ api/data/seeds/02-data.js
@@ -1,9 +1,9 @@
 exports.seed = async function (knex) {
     await knex('users').truncate()
     await knex('plants').truncate()
-    await knex('users').insert([
-//can add dummy user data if needed or wanted
-    ])
+//     await knex('users').insert([
+// //can add dummy user data if needed or wanted
+//     ])
     await knex('plants').insert([
       {
         nickname: 'Bamboo',
